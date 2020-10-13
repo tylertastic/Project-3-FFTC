5dd65244ce8f04ff3800b9229cb04372976a59d4
Tyler Williams <tyler.williams5471235@gmail.com>
2020-10-13 04:11:09 -0500

    wrote skeleton subroutines

---
+++ koch_curve.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+level = 5
+r = 0.29
+
+def x_coords(level):
+    
+
+
+# Draw line at lowest level of recursion
+def subroutine_1(level):
+    if level > 1:
+        subroutine_2(level)
+    plt.plot()
+    
+# Branch into lower levels
+def subroutine_2(level):
+    pass
+
+# Return
+def subroutine_3():
+    pass
+    
+def main():
+    pass
+    
+  
