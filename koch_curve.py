14f91c31dab477b0be56549a87e600096081c625
Tyler Williams <tyler.williams5471235@gmail.com>
2020-10-15 06:11:54 -0500

    added a W operator, stuck on 'matrixifying' W if possible

--- koch_curve.py
+++ koch_curve.py
@@ -82,7 +82,10 @@ settings = np.array([[1/3, 0, 0, 0],
             [1/3, np.pi/3, 1/3, 0],
             [1/3, -np.pi/3, 1/2, np.sqrt(3)],
             [1/3, 0, 2/3, 0]])
-initiator = [[0,1],[0,0]]
+initiator = np.array([[0,1],
+                      [0,0]])
+matrix_ary = [] # iterates?
+    
 
 def rotation_mat(thta):
     return np.array([[np.cos(thta), -np.sin(thta)],
@@ -96,7 +99,8 @@ def Hutch_w1(v):
     ty = w1[3]
     
     rot = rotation_mat(thta)
-    trans = np.array([tx, ty])
+    trans = np.array([[tx, tx],
+                      [ty, ty]])
     
     operator = s*(rot @ v) + trans
     return operator
@@ -109,7 +113,8 @@ def Hutch_w2(v):
     ty = w2[3]
     
     rot = rotation_mat(thta)
-    trans = np.array([tx, ty])
+    trans = np.array([[tx, tx],
+                      [ty, ty]])
     
     operator = s*(rot @ v) + trans
     return operator
@@ -122,7 +127,8 @@ def Hutch_w3(v):
     ty = w3[3]
     
     rot = rotation_mat(thta)
-    trans = np.array([tx, ty])
+    trans = np.array([[tx, tx],
+                      [ty, ty]])
     
     operator = s*(rot @ v) + trans
     return operator
@@ -135,19 +141,40 @@ def Hutch_w4(v):
     ty = w4[3]
     
     rot = rotation_mat(thta)
-    trans = np.array([tx, ty])
+    trans = np.array([[tx, tx],
+                      [ty, ty]])
     
     operator = s*(rot @ v) + trans
     return operator
 
-
-# this is not right but its presence shows something
-def iterate_wk(initiator, iterations, wk):
-    if iterations = 1:
-        return initiator
+# Let A_0 be the initiator and recursively define
+# A_(k+1) = W(A_k)
+# where W(A) = w1(A) U ... U w4(A)
+def Hutch_W(v, n):
+    if n == 0:
+        matrix_ary.append(v)
+        return np.array(matrix_ary)
     else:
-        initiator = [wk(initiator[0]), wk(initiator[1])]
-        return compute_new_line_segment(initiator, iterations-1, wk)
+        matrix_ary.append(Hutch_w1)
+        return Hutch_W([np.array(matrix_ary.append(Hutch_w1(Hutch_W(v, n-1)))),
+                        np.array(matrix_ary.append(Hutch_w2(Hutch_W(v, n-1)))),
+                        np.array(matrix_ary.append(Hutch_w3(Hutch_W(v, n-1)))),
+                        np.array(matrix_ary.append(Hutch_w3(Hutch_W(v, n-1))))], 0)
 
+# Model
+# Func 1 : compute left branch from higher order left branches
+# func 2 : computer middle left branch from higher order middle left branches
+# func 3 : ...
+# func 4 : ...
 
+print(Hutch_W(initiator, 0))
+
+# # returns filled array of matrices containing points to draw lines between
+# def fill_matrix_ary(iterations):
+#     if iterations == 1:
+#         matrix_ary.append(initiator)
+#         return matrix_ary
+#     else:
+        
+        
     
